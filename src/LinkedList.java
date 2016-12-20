4ac383857290db794fb6a8d2c9c3a75bf166889c
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-20 01:47:16 -0500

    initial commit

---
+++ src/LinkedList.java
@@ -0,0 +1,13 @@
+/**
+ * Created by shant on 20-12-2016.
+ */
+public class LinkedList {
+    Node node;
+    class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+        }
+    }
+}
