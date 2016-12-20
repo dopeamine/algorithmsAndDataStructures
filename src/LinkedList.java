85d86a21aac480d2f1d790d1b500af7aa5237944
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-20 18:13:35 -0500

    linked list implementation, push, insertafter, append methods

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -12,4 +12,40 @@ public class LinkedList {
             next = null;
         }
     }
+    public void printList(){
+        Node node = head;
+        while (node != null){
+            System.out.println(node.data);
+            node = node.next;
+        }
+    }
+    public void push(int newData){
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void insertAfter(Node prevNode, int newData){
+        if (prevNode == null){
+            System.out.println("Given previous node cannot be null");
+            return;
+        }
+        Node newNode = new Node(newData);
+        newNode.next = prevNode.next;
+        prevNode.next = newNode;
+    }
+
+    public void append(int newData){
+        Node newNode = new Node(newData);
+        if (head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = null;
+        Node last = head;
+        while (last.next != null)
+            last = last.next;
+
+        last.next = newNode;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,26 @@ public class Main {
         LinkedList linkedList=new LinkedList();
         linkedList.head= new LinkedList.Node(1);
         LinkedList.Node second = new LinkedList.Node(2);
-        LinkedList.Node third = new LinkedList.Node(3);
+        LinkedList.Node third = new LinkedList.Node(4);
+        linkedList.head.next = second;
+        second.next = third;
+
+        System.out.println("Printing newly created linked list");
+        linkedList.printList();
+
+        System.out.println("Pushing element in linked list");
+        //push node
+        linkedList.push(0);
+        linkedList.printList();
+
+        System.out.println("Inserting node after given node");
+        //insert node after given node
+        linkedList.insertAfter(second,3);
+        linkedList.printList();
+
+        System.out.println("Appending new node at end of list");
+        //append - at end of linked list
+        linkedList.append(5);
+        linkedList.printList();
     }
 }
