f5a75acc33758ab24fbf46f458f1d004a1921bd8
dopeamine <shantanu.rajenimbalkar@gmail.com>
2016-12-20 12:56:49 -0500

    simple linked list, created 3 linked list nodes

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -2,12 +2,14 @@
  * Created by shant on 20-12-2016.
  */
 public class LinkedList {
-    Node node;
-    class Node{
+    Node head;
+/* Linked list Node.  This inner class is made static so that main() can access it */
+    static class Node{
         int data;
         Node next;
         Node(int d){
             data = d;
+            next = null;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        LinkedList linkedList=new LinkedList();
+        linkedList.head= new LinkedList.Node(1);
+        LinkedList.Node second = new LinkedList.Node(2);
+        LinkedList.Node third = new LinkedList.Node(3);
     }
 }
